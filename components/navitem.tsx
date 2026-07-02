15042c823859680c44b821cc6ed86b9dc9cde9cd
notHaruu <harukanito4717@gmail.com>
2026-07-02 15:08:34 +0200

    Enhanced NavItem component to highlight the active link by applying a gradient background based on the current pathname. Improved hover effects for better user interaction.

--- components/navitem.tsx
+++ components/navitem.tsx
@@ -2,6 +2,7 @@
 
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 export default function NavItem({
@@ -13,16 +14,19 @@ export default function NavItem({
   label: string;
   icon?: ReactNode;
 }) {
+
+  const path = usePathname();
+
   return (
     <Link href={href}>
       <motion.div
         whileHover={{ scale: 1.05, y: -1 }}
         transition={{ type: "spring", stiffness: 300, damping: 20 }}
-        className="px-3 py-2 rounded-lg cursor-pointer hover:bg-neutral-300 dark:hover:bg-neutral-700 flex items-center gap-3"
+        className={`px-3 py-2 rounded-lg cursor-pointer hover:bg-blue-200 lg:hover:bg-neutral-300 lg:dark:hover:bg-neutral-700 flex items-center gap-3 ${path === href && "bg-gradient-to-t from-green-500 to-lime-400"}`}
       >
         {icon}
 
-        <motion.span whileHover={{ x: 6 }} className="text-black dark:text-white">
+        <motion.span whileHover={{ x: 6 }}>
           {label}
         </motion.span>
       </motion.div>
