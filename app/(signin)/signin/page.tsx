6961debc41ba00f1744a2fed225c7ef89cd9d811
notHaruu <harukanito4717@gmail.com>
2026-07-02 15:24:40 +0200

    Updated styling for Profile and SignIn components to enhance layout and responsiveness. Adjusted mobile navigation button to include a cursor pointer for better user experience. Improved hover effects in NavItem component for consistent interaction design.

--- app/(signin)/signin/page.tsx
+++ app/(signin)/signin/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <div className="p-5">
-      <div className="p-20">
-        <h1 className="mt-20 text-8xl text-center pb-10 mb-10 font-bold border-b-2 border-neutral-200 dark:border-neutral-700 theme-transition">Spotify Analytics</h1>
+      <div className="p-4 lg:p-20">
+        <h1 className="mt-20 text-6xl lg:text-8xl text-center pb-10 mb-10 font-bold border-b-2 border-neutral-200 dark:border-neutral-700 theme-transition">Spotify Analytics</h1>
         <div className="pt-7 flex justify-center scale-150">
           <SignIn />
         </div>
--- components/navitem.tsx
+++ components/navitem.tsx
@@ -22,7 +22,7 @@ export default function NavItem({
       <motion.div
         whileHover={{ scale: 1.05, y: -1 }}
         transition={{ type: "spring", stiffness: 300, damping: 20 }}
-        className={`px-3 py-2 rounded-lg cursor-pointer hover:bg-blue-200 lg:hover:bg-neutral-300 lg:dark:hover:bg-neutral-700 flex items-center gap-3 ${path === href && "bg-gradient-to-t from-green-500 to-lime-400"}`}
+        className={`px-3 py-2 rounded-lg cursor-pointer hover:bg-neutral-300 dark:hover:bg-neutral-700 flex items-center gap-3 ${path === href && "bg-gradient-to-t from-green-500 to-lime-400"}`}
       >
         {icon}
 
